8e56d2d70bf8bf16c4ed714f112d3dd6fd75c42e
Daniel F <daniel.fernandes.santos19@escola.pr.gov.br>
2025-09-16 12:16:26 -0300

    ez

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 const sons = {
-    'A' : '',
+    'A' : 'boom.wav',
+    'S' : 'clap.wav',
+    'D' : 'hithat.wav',
+    'F' : 'hithat.wav',
+    'G' : 'kick.wav',
+    'H' : 'openhat.wav',
+    'I' : 'ride.wav',
+    'J' : 'snare.wav',
+    'K' : 'tink.wav',
+    'L' : 'tom.wav',
 }
+//Função para criar uma div para cada tecla
+const criarDiv = (sons) => {
+    div.classlist('key')
+    div.textcontent = texto;
+    
+}
+
+exebir(sons);
+document.getElementById('container')
